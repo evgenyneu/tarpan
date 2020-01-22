6621f8aabccc5d2610e407c70bb6654b63df025b
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-01-22 17:29:20 +1100

    Setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tarpan-evgenyneu",
+    version="0.0.1",
+    author="Evgenii Neumerzhitckii",
+    author_email="sausageskin@gmail.com",
+    description="A collection of tools for analysing cmdspanpy output, written in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/evgenyneu/tarpan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
