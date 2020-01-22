4018359e96525c903cd92bb3edd7b64050d3648f
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-01-23 10:08:41 +1100

    Work

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tarpan",
-    version="0.1.1",
+    version="0.1.2",
     author="Evgenii Neumerzhitckii",
     author_email="sausageskin@gmail.com",
     description="A collection of tools for analysing cmdspanpy output, written in Python",
--- tarpan/__init__.py
+++ tarpan/__init__.py
@@ -1,4 +1,1 @@
-from .shared import (
-    save_summary, TreePlotParams, save_posterior_plot,
-    InfoPath, get_info_path, summary_from_dict,
-    make_comparative_tree_plot,)
+
