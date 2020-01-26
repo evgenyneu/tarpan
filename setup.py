849b07f63d0b4a7b854cdee413abfab9f1ed3514
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-01-26 16:52:45 +1100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tarpan",
-    version="0.2.0",
+    version="0.2.1",
     author="Evgenii Neumerzhitckii",
     author_email="sausageskin@gmail.com",
     description="A collection of tools for analysing cmdspanpy output, written in Python",
