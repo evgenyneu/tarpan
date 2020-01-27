8e06591e11843ea67b356d8686fa01b7a872f072
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-01-27 13:20:39 +1100

    Used specific library versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tarpan",
-    version="0.2.3",
+    version="0.2.4",
     author="Evgenii Neumerzhitckii",
     author_email="sausageskin@gmail.com",
     description="A collection of tools for analysing cmdspanpy output, written in Python",
@@ -14,14 +14,14 @@ setuptools.setup(
     url="https://github.com/evgenyneu/tarpan",
     packages=setuptools.find_packages(),
     install_requires=[
-        'cmdstanpy==0.8.0',
-        'scipy==1.4.1',
-        'tabulate==0.8.6',
-        'pandas==0.25.3',
-        'numpy==1.18.1',
-        'arviz==0.6.1',
-        'matplotlib==3.1.2',
-        'seaborn==0.10.0'
+        'cmdstanpy>=0.8.0,<0.9.0',
+        'scipy>=1.4.1,<2.0.0',
+        'tabulate>=0.8.6,<0.9.0',
+        'pandas>=0.25.3,<0.26.0',
+        'numpy>=1.18.1,<2.0.0',
+        'arviz>=0.6.1,<0.7.0',
+        'matplotlib>=3.1.2,<4.0.0',
+        'seaborn>=0.10.0,<0.11.0'
     ],
     tests_require=[
         'pytest==5.3.0'
