7c8b8a44b2288060f609b972b69dee0b01bb814d
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-01-31 14:07:43 +1100

    Bimp version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tarpan",
-    version="0.2.7",
+    version="0.2.8",
     author="Evgenii Neumerzhitckii",
     author_email="sausageskin@gmail.com",
     description="A collection of tools for analysing cmdspanpy output, written in Python",
