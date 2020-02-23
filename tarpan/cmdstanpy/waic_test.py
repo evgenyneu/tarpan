27ea65e91bdb0459733a3a70ca6fe727856e092d
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-02-23 15:47:52 +1100

    Work

--- tarpan/cmdstanpy/waic_test.py
+++ tarpan/cmdstanpy/waic_test.py
@@ -3,3 +3,4 @@ from tarpan.testutils.a03_cars.cars import get_fit
 
 def test_hello():
     fit = get_fit()
+    print(fit)
--- tarpan/testutils/a03_cars/cars.py
+++ tarpan/testutils/a03_cars/cars.py
@@ -2,15 +2,19 @@
 
 from cmdstanpy import CmdStanModel
 import os
+import pandas as pd
 from tarpan.cmdstanpy.cache import run
 from tarpan.shared.info_path import InfoPath
 
 
 def get_data():
+    csv_path = "tarpan/testutils/a03_cars/cars.csv"
+    df = pd.read_csv(csv_path)
+
     return {
-        "J": 8,
-        "y": [28,  8, -3,  7, -1,  1, 18, 12],
-        "sigma": [15, 10, 16, 11,  9, 11, 10, 18]
+        "n": len(df.index),
+        "dist": df["dist"].tolist(),
+        "speed": df["speed"].tolist()
     }
 
 
@@ -19,11 +23,11 @@ def run_model(data, output_dir):
     Runs Stan model and saves fit to disk
     """
 
-    model_path = "tarpan/testutils/a01_eight_schools/eight_schools.stan"
+    model_path = "tarpan/testutils/a03_cars/stan_model/cars.stan"
     model = CmdStanModel(stan_file=model_path)
 
-    return model.sample(data=data, chains=4, cores=4,
-                        sampling_iters=1000, warmup_iters=1000,
+    return model.sample(data=data, chains=1, cores=1,
+                        sampling_iters=1000, warmup_iters=500,
                         output_dir=output_dir)
 
 
@@ -34,28 +38,9 @@ def get_fit():
 
     info_path = InfoPath(
                     path='temp_data',
-                    dir_name="a01_eight_schools",
+                    dir_name="a03_cars",
                     sub_dir_name=InfoPath.DO_NOT_CREATE
                 )
 
-    return run(info_path=info_path, func=run_model, data=get_data())
-
-
-def get_fit_larger_uncertainties():
-    """
-    Returns fit file for unit tests, uses data with larger uncertainties.
-    """
-
-    info_path = InfoPath(
-                    path='temp_data',
-                    dir_name="a01_eight_schools_large_uncert",
-                    sub_dir_name=InfoPath.DO_NOT_CREATE
-                )
-
-    run(info_path=info_path, func=run_model, data=get_data())
-
-    # Use data with increased uncertainties
     data = get_data()
-    data["sigma"] = [u * 2 for u in data["sigma"]]
-
     return run(info_path=info_path, func=run_model, data=data)
