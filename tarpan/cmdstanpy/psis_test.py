23968cf7c53cb8e7f38fb53e2e9811736bfa552b
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2020-02-26 17:19:42 +1100

    Work

--- tarpan/cmdstanpy/psis_test.py
+++ tarpan/cmdstanpy/psis_test.py
@@ -1,4 +1,5 @@
 import warnings
+from pytest import approx
 
 from tarpan.cmdstanpy.psis import (
     psis)
@@ -13,4 +14,15 @@ def test_psis():
 
     result = psis(fit)
 
-    
+    assert result.psis == approx(126.20, rel=1e-4)
+    assert result.psis_std_err == approx(12.867, rel=1e-4)
+    assert result.penalty == approx(3.8525, rel=1e-4)
+
+    assert len(result.psis_pointwise) == 50
+    assert result.psis_pointwise[0] == approx(4.1269, rel=1e-4)
+    assert result.psis_pointwise[49] == approx(1.9442, rel=1e-4)
+
+    assert len(result.pareto_k) == 50
+    assert result.pareto_k[0] == approx(0.0048670, rel=1e-4)
+    assert result.pareto_k[12] == approx(0.80011, rel=1e-4)
+    assert result.pareto_k[49] == approx(0.28713, rel=1e-4)
